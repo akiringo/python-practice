5e3b7f26575829a0eb40eaa7f45b9dbfed805fc6
Kevin O'Neil <kevinmoneil@me.com>
2018-10-27 22:31:52 +0900

    loops through each page and gets the english name of the item

--- web-scrape.py
+++ web-scrape.py
@@ -1,7 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+
+
 url = 'https://www.enherb.jp/consept/blend/p-e130035-blend.html'
 r = requests.get(url)
 soup = BeautifulSoup(r.text,'html.parser')
+contents_list = soup.find('div', class_='contents_list')
 
-print(soup.find_all('a'))
+for content in contents_list.find_all('a'):
+  link = content['href']
+  new_link_content = requests.get(link)
+  new_content = BeautifulSoup(new_link_content.text, 'html.parser')
+  stuff = new_content.find(text='英名') 
+  print(stuff.parent.parent.next_sibling.text)
+
